95ee5cac17c7b082dfded9c94b64f6b654b26a4f
ryan <1184518756@qq.com>
2020-09-25 16:36:35 +0800

    update interface

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,3 +7,5 @@ export function Extra(): void;
 export function NativeApi(): void;
 
 export function Props(): void;
+
+export function Watch(params?: { property?: string, deep?: boolean, immediate?: boolean } | string): void;
